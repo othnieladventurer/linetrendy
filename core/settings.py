de5581e7ecd50b724d4d654a3baceee1ce12b8d1
othnieladventurer <thefreakbeatz@gmail.com>
2025-07-28 12:28:09 -0400

    second commit

--- core/settings.py
+++ core/settings.py
@@ -12,9 +12,20 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+os.environ['TZ'] = 'America/New_York'
+import environ
+import dj_database_url
+from decouple import config
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+ENVIRONMENT= env('ENVIRONMENT', default='production')
+SERVICE_ACCOUNT_FILE = os.path.join(BASE_DIR, 'credentials', 'service_account.json')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
